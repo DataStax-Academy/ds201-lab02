fc90718c6b191cc64d0e7764b892df09e2ac3ce7
David Chung <dchung@dchung.com>
2023-04-24 11:01:45 -0500

    changed start C* and wait.sh

--- wait.sh
+++ wait.sh
@@ -10,6 +10,7 @@ echo " ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚�
 echo ""
 
 echo -n 'Preparing your environment ...'; 
+timeout 60 bash -c 'until cqlsh -e "describe cluster" >/dev/null 2>&1; do sleep 2; echo -n "."; done';
 echo ' DONE!'
 echo "Environment ready."
 
